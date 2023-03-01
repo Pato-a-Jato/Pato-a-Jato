e9f7915ac4fccef16ca4b18233c79b2260a4445e
satilpereira <satil.prog@gmail.com>
2023-02-28 21:31:27 -0300

    add photo

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,7 +1,10 @@
 const btnMenu = document.getElementById('btn-menu');
 const menu = document.getElementById('menu');
+let state = false;
 
 btnMenu.addEventListener('click', (e) => {
     menu.classList.toggle('active');
+    state ? btnMenu.style.position = 'absolute' : btnMenu.style.position = 'fixed';
+    state = !state;
 })
 
